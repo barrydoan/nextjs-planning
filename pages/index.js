53f3276803e0e9cc8c17c1eb8013095f1baade18
Barry Doan <barry.n.doan@gmail.com>
2021-05-21 07:48:06 -0400

    Add list page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return <div>
-    <h1>This is homepage</h1>
+    <EventList items={featuredEvents}/>
   </div>
 }
 
